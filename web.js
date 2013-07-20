07d27615d592ea8244dfb1e96a090cc9fbb34650
Tiago Jost <tiagotrevisanjost@gmail.com>
2013-07-20 17:50:09 +0000

    Updated to index web.js

--- web.js
+++ web.js
@@ -1,12 +1,7 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+var string = fs.readFileSync("index.html");
+var buf  = new Buffer(string);
+string  = buf.toString(buf);
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+console.log(string);
